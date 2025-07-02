7fceee047ef6aff4aa38cd559a9bf9a9df846ba8
naattxzw <souza.nathalia23@escola.pr.gov.br>
2025-07-02 00:58:32 +0000

    Finalizado

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o melhor Goleiro do Mundo?',
+    'Manuel Neuer'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o Menor país do mundo?',
+    'Roma'
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi descoberto o Brasil?',
+    'Foi descoberto em 1500'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Pai em Inglês?',
+    'Pai em inglês se diz, Dad'
+)
